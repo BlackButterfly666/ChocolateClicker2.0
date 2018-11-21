4e21efb988494ea1b8d9e0bf210b3b7b76496643
Anne <3dgame.umschulung@gmail.com>
2018-11-21 14:46:43 +0100

    first commit, unfinished

---
+++ Assets/Scripts/IngredientManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static Texts.Names;
+
+public class IngredientManager : MonoBehaviour
+{
+	public IngredientEvent IngredientMessage = new IngredientEvent();
+
+	//Stage I - Küche
+	[SerializeField] Button cacaoButton;
+	[SerializeField] Ingredient cacao;
+	int cacaoTotal;
+	[SerializeField] Button sugarButton;
+	[SerializeField] Ingredient sugar;
+	int sugarTotal;
+	[SerializeField] Button milkButton;
+	[SerializeField] Ingredient milk;
+	int milkTotal;
+	[SerializeField] Button butterButton;
+	[SerializeField] Ingredient butter;
+	int butterTotal;
+	[SerializeField] Button creamButton;
+	[SerializeField] Ingredient cream;
+	int creamTotal;
+	Product darkChocolate;
+
+	void Start()
+	{
+		NewIngredient();
+		ClickListenerAdd();
+	}
+
+	void Update()
+	{
+		Debug.Log("Name: " + cacao.Name + "Amount per Click: " + cacao.AmountPerClick + "Total: " + cacao.Total);
+	}
+
+	/*public void OnClick()
+	{
+		cacaoTotal += cacao.AmountPerClick;
+	}*/
+
+	public void NewIngredient()
+	{
+		//Stage I - Küche
+		//new Ingredient (string name, int amountPerClick, int amountPerSecond
+		cacao = new Ingredient(tCacao, 1, 0,ref cacaoTotal);
+		sugar = new Ingredient(tSugar, 1, 0, ref sugarTotal);
+		milk = new Ingredient(tMilk, 1, 0, ref milkTotal);
+		butter = new Ingredient(tButter, 1, 0, ref butterTotal);
+		cream = new Ingredient(tCream, 1, 0, ref creamTotal);
+	}
+
+	public void NewProduct()
+	{
+		darkChocolate = new Product("Dark Chocolate", 1, 0, new Ingredient[] { cacao, sugar  });
+	}
+
+	public void ClickListenerAdd()
+	{
+		//Stage I - Küche
+		cacaoButton.onClick.AddListener(SetCacao);
+		//sugarButton.onClick.AddListener(SetSugar);
+		//milkButton.onClick.AddListener(SetMilk);
+		//butterButton.onClick.AddListener(SetButter);
+		//creamButton.onClick.AddListener(SetCream);
+	}
+
+	//Stage I - Küche
+	public void SetCacao() { IngredientMessage.Invoke(cacao); }
+	public void SetSugar() { IngredientMessage.Invoke(sugar); }
+	public void SetMilk() { IngredientMessage.Invoke(milk); }
+	public void SetButter() { IngredientMessage.Invoke(butter); }
+	public void SetCream() { IngredientMessage.Invoke(cream); }
+}
---
+++ Assets/Scripts/Stock.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Stock : MonoBehaviour
+{
+	Ingredient secretIngredient;
+
+	public void SetIngredient(Ingredient ingredient)
+	{
+		secretIngredient = ingredient;
+		secretIngredient.Total += secretIngredient.AmountPerClick + secretIngredient.AmountPerSecond;
+	}
+}
+
+//Boilerplate Code für Unity um ein ein Event mit Custom Data zu bekommen
+[System.Serializable]
+public class IngredientEvent : UnityEvent<Ingredient> { }
+
+//Serializable und SerializeField, um die Zutaten im Editor sichtbar zu machen
+[System.Serializable]
+public struct Ingredient
+{
+	//private Backing-Varable und public Property
+	[SerializeField] string name;
+	public string Name { get { return name; } }
+
+	[SerializeField] int amountPerClick;
+	public int AmountPerClick { get { return amountPerClick; } set { amountPerClick = value; } }
+
+	int total;
+	public int Total { get { return total; } set { total = value; } }
+
+	[SerializeField] int amountPerSecond;
+	public int AmountPerSecond { get { return amountPerSecond; } set { amountPerSecond = value; } }
+
+	//Constructor
+	public Ingredient(string name, int amountPerClick, int amountPerSecond, ref int total)
+	{
+		this.name = name;
+		this.amountPerClick = amountPerClick;
+		this.amountPerSecond = amountPerSecond;
+		this.total = total;
+	}
+}
+
+public class Product
+{
+	//private Backing-Varable und public Property
+	[SerializeField] string name;
+	public string Name { get { return name; } }
+
+	[SerializeField] int amountPerClick;
+	public int AmountPerClick { get { return amountPerClick; } set { amountPerClick = value; } }
+
+	[SerializeField] int total = 0;
+	public int Total { get { return total; } set { total = value; } }
+
+	[SerializeField] int amountPerSecond;
+	public int AmountPerSecond { get { return amountPerSecond; } set { amountPerSecond = value; } }
+
+	Ingredient[] ingredients;
+
+	//Constructor
+	public Product(string name, int amountPerClick, int amountPerSecond, Ingredient[] ingredients)
+	{
+		this.name = name;
+		this.amountPerClick = amountPerClick;
+		this.amountPerSecond = amountPerSecond;
+		this.ingredients = ingredients;
+
+		//ToDo => recipe ingredients
+	}
+}
